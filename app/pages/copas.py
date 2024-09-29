85ea0a2f6adbb0ad5daf0311fe9c55f8fdc5fa8e
renoaldo <renoaldo@hotmail.com>
2024-09-29 15:07:37 -0300

    adiciona eventos e estatisticas

--- app/pages/copas.py
+++ app/pages/copas.py
@@ -115,9 +115,10 @@ def run():
     
     
     
+    
     referee = df_match[df_match['match_id'] == match_id]['referee'].values[0]
     
-    with st.container():
+    with st.container(border=True):
         st.title(f"{match['home_team']} x {match['away_team']}")
         st.subheader(f"{match_date} - {hora}")
         
@@ -140,17 +141,24 @@ def run():
             
             st.markdown(f"<h3 style='color: red'>{away_team} (Visitante)</h3>", unsafe_allow_html=True)
             st.markdown(f"<h1 style='color: red'>{away_score}</h1>", unsafe_allow_html=True)   
-    
-    st.divider()
-    st.header("Informações e Detalhes")
-    tab1, tab2, tab3 = st.tabs(["Escalação", "Detalhes da Partida", "Estatísticas"])
     # ========================== Escalação ==========================
+    st.header("Informações e Detalhes")
+    st.write("")
+    tab1, tab2, tab3 = st.tabs(["Estatísticas", "Detalhes da Partida", "Escalação"])
+    
     mkd_text("", level='subheader')
     home_lineup = sb.lineups(match_id=match_id)[home_team].sort_values('jersey_number')
     away_lineup = sb.lineups(match_id=match_id)[away_team].sort_values('jersey_number')
-    with tab1:
     
-        with st.expander("👥 Jogadores",expanded=True):
+    # Tecnicos
+    home_managers = df_match['home_managers'].values[0]
+    away_managers = df_match['away_managers'].values[0]
+    
+    
+    with tab3:
+        with st.container(border=True):
+            st.subheader("Jogadores")
+            #with st.expander("👥 Jogadores",expanded=True):
             col3 = st.columns([1,1])
             with col3[0]:
                 st.markdown(f"<h3 style='color: green'>{home_team} (Casa)</h3>", unsafe_allow_html=True)
@@ -170,11 +178,11 @@ def run():
                     
                     # Exibir o resultado no Streamlit
                     st.write(df_away)
-    
-    # ========================== Detalhes da Partida ==========================
     with tab2:
-        with st.expander("🏟️👔 Partida e Técnicos:", expanded=True):
-            #st.subheader("Detalhes da Partida")
+        with st.container(border=True):
+        # ========================== Detalhes da Partida ==========================
+            #with st.expander("🏟️👔 Partida e Técnicos:", expanded=True):
+            st.subheader("Detalhes da Partida")
             st.write(f"**Estádio:** {match['stadium']}")
             # match_week
             st.write(f"**Semana:** {match['match_week']}")
@@ -184,20 +192,85 @@ def run():
             st.subheader("Técnicos")
             col3 = st.columns([1,1])
             with col3[0]:
-                # Tecnicos
-                home_managers = df_match['home_managers'].values[0]
                 st.markdown(f"<h3 style='color: green'>{home_team}</h3>", unsafe_allow_html=True)
                 st.markdown(f"<h5 >{home_managers}</h5>", unsafe_allow_html=True)
             with col3[1]:
-                away_managers = df_match['away_managers'].values[0]
                 st.markdown(f"<h3 style='color: red'>{away_team}</h3>", unsafe_allow_html=True)
                 st.markdown(f"<h5 >{away_managers}</h5>", unsafe_allow_html=True)    
-    
+    with tab1:
+        with st.container(border=True):
+            st.subheader("Estatísticas")
+            col5 = st.columns([1, 1,1])
+            with col5[1]:
+                visao = st.radio("", [f"Casa","Geral", "Visitante"], horizontal=True, index=1)
+            
+            events = filter_events(visao, match_id, home_team, away_team)
+            #col5 = 
+            col4 = st.columns([1, 1, 1, 1,1,1])
+            with col4[0]:
+                st.metric("Passes", events['type'].value_counts().get('Pass', 0))
+            with col4[1]:
+                shots = events['type'].value_counts().get('Shot', 0)
+                st.metric("Chutes a Gol", shots)
+            with col4[2]:
+                if visao == "Casa":
+                    goals = home_score
+                if visao == "Visitante":
+                    goals = away_score
+                if visao == "Geral":
+                    goals = home_score + away_score
+                percent_score = round((goals / shots) * 100,2)
+                st.metric("Conversão(Gols)", f'{percent_score}%')
+            with col4[3]:
+                st.metric("Faltas", events['type'].value_counts().get('Foul Committed', 0))
+            with col4[4]:
+                st.metric("Dribles", events['type'].value_counts().get('Dribble', 0))
+            with col4[5]:  # Add an additional column for corner kicks
+                corner_passes = events[(events['type'] == 'Pass') & (events['pass_type'] == 'Corner')]
+                corner_shots = events[(events['type'] == 'Shot') & (events['play_pattern'] == 'From Corner')]
+                corner = len(corner_passes) + len(corner_shots)
+                st.metric("Escanteios", corner)
 
 
-    
+            # Selecionar colunas principais, como jogador, time, tipo de evento e métricas específicas de cada tipo
+            event_columns = [
+                'type',            # Tipo de evento (Pass, Shot, Duel, etc.)
+                'minute',          # Minuto do evento
+                'second',          # Segundo do evento
+                'team',            # Nome do time
+                'player',          # Nome do jogador
+                'location',        # Localização do evento no campo
+                'pass_end_location',  # Fim do passe (se for um passe)
+                'pass_length',     # Comprimento do passe (se for um passe)
+                'pass_outcome',    # Resultado do passe (se for um passe)
+                'shot_outcome',    # Resultado do chute (se for um chute)
+                'shot_statsbomb_xg',  # xG do chute (se for um chute)
+                'shot_technique',  # Técnica do chute (se for um chute)
+                'duel_outcome',    # Resultado do duelo (se for um desarme)
+            ]
+
+            # Filtrando apenas as colunas que estão disponíveis nos eventos
+            events_filtered = events[event_columns].copy()
+
+            # Exibindo o DataFrame no Streamlit
+            st.write("")
+            st.header("Eventos")
+            st.dataframe(events_filtered, hide_index = True)
+
+        # Exemplo de algumas métricas simples que podem ser calculadas a partir do DataFrame
+        total_passes = len(events[events['type'] == 'Pass'])
+        total_shots = len(events[events['type'] == 'Shot'])
+        total_tackles = len(events[(events['type'] == 'Duel') & (events['duel_type'] == 'Tackle')])
+
+        # Exibindo métricas relacionadas aos eventos
+        st.metric("Total de Passes", total_passes)
+        st.metric("Total de Finalizações", total_shots)
+        st.metric("Total de Desarmes", total_tackles)
 
     
+
+        st.dataframe(events)
+
     
     #st.write(f"**Cidade:** {match['city']}")
     #st.write(sb.lineups(match_id=match_id))
@@ -207,6 +280,20 @@ def run():
 
 
     #st.write(sb.matches(competition_id=43, season_id=season_id))
+
+
+def filter_events(visao, match_id, home_team, away_team):
+    if visao == "Casa":
+        events = sb.events(match_id=match_id, flatten_attrs=True)
+        events = events[events['team'] == home_team]
+    elif visao == "Visitante":
+        events = sb.events(match_id=match_id, flatten_attrs=True)
+        events = events[events['team'] == away_team]
+    else:
+        events = sb.events(match_id=match_id, flatten_attrs=True)
+    return events
+
+
 
 if __name__ == "__main__":
     run()
